70ee9c33a34a4e22dd7810b8d49ca9fe97ba1b5c
EricEE1022 <ebidinho@gmail.com>
2022-02-01 11:44:55 +0100

    fix

--- src/pages/dealRoom/dealRoom.js
+++ src/pages/dealRoom/dealRoom.js
@@ -10,8 +10,8 @@ export const StartupDealRoom = () => {
           <img src={comingSoon} alt="" />
           <h3 className="py-5">Feature Under Construction</h3>
           <p>
-            We know you really want to use this feature and we are <br />{' '}
-            excited to show you how awesomes it is but unfortunately it’s <br />{' '}
+            We know you really want to use this feature and we are <br />
+            excited to show you how awesomes it is but unfortunately it’s <br />
             still under construction. Please exercise a little patient.
           </p>
         </div>
