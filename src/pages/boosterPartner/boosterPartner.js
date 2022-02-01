29459cafd95d0ddb3053d7fb24f9bb506684c589
EricEE1022 <ebidinho@gmail.com>
2022-02-01 12:39:24 +0100

    fix

--- src/pages/boosterPartner/boosterPartner.js
+++ src/pages/boosterPartner/boosterPartner.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React from 'react'
 import {} from './boosterPartner.styled'
+import { DashCard } from '../../components/index'
+import { cardData } from '../../constants/domiData'
 
 export const BoosterPartner = () => {
-    return (
-        <div>Booster Partner</div>
-    )
+  return (
+    <div>
+      <section className="row" style={{ zIndex: '999', background: '#FEFEFE' }}>
+        {cardData.map((data, i) => (
+          <>
+            <DashCard
+              className="col-lg-3 col-md-6 col-12"
+              icon={data.icon}
+              name={data.name}
+              count={data.count}
+              color={data.color}
+              key={i}
+            />
+          </>
+        ))}
+      </section>
+    </div>
+  )
 }
