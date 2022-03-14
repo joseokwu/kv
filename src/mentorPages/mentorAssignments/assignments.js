ad28811e2989b08ff922936ef23d8d45cc4feb58
EricEE1022 <ebidinho@gmail.com>
2022-03-14 18:38:41 +0100

    fix

--- src/mentorPages/mentorAssignments/assignments.js
+++ src/mentorPages/mentorAssignments/assignments.js
@@ -5,12 +5,9 @@ import "./assignments.css";
 import { useHistory } from "react-router-dom";
 import { mentorAssignments } from "../../services";
 import { PageLoader } from "../../components";
-import { AssignmentCard } from './../../Startupcomponents/assignmentCard/assignmentCard';
 
 export const MentorAssignments = () => {
-
   const { push } = useHistory();
-
 
   const [assignments, setAssignments] = useState([]);
   const [cards, setCards] = useState([]);
@@ -102,6 +99,29 @@ export const MentorAssignments = () => {
             );
           })}
       </section>
+    </div>
+  );
+};
+export const AssignmentCard = ({ data = {} }) => {
+  const { push } = useHistory();
+
+  return (
+    <div className="assignment_card opp-card my-3">
+      <h3>{data?.topic}</h3>
+      <p className="pt-2 pb-4 border-bottom">
+        Attachments - <span>businessplan.pdf</span>
+      </p>
+
+      <p className="pt-3 pb-3 border-bottom">
+        {data?.description}   
+        <span onClick={() => push('/mentor/assignments/create/details')}>More Details</span>
+      </p>
+      <button
+        className="pending_evaluation mt-4"
+        onClick={() => push("/mentor/assignments/view")}
+      >
+        View Assignment
+      </button>
     </div>
   );
 };
