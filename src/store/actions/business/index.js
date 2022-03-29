37c2c1e8de4c77e661b863463130fb07eac96fd5
Jose <38558352+joseokwu@users.noreply.github.com>
2022-03-29 18:04:43 +0100

    work experience

--- src/store/actions/business/index.js
+++ src/store/actions/business/index.js
@@ -1,49 +1,55 @@
 import {
-    DASHBOARD, WALLET,
-    TRANSACTIONS, DATA_BANK,
-    CHART_SUPPORT, SETTINGS,
-    CHANGE_PAGE,
-    SHOW_EVENT,
-    GET_EVENTS_FAILED,
-    GET_EVENTS_SUCCESS
-  } from '../actions.types';
+  DASHBOARD,
+  WALLET,
+  TRANSACTIONS,
+  DATA_BANK,
+  CHART_SUPPORT,
+  SETTINGS,
+  CHANGE_PAGE,
+  SHOW_EVENT,
+  GET_EVENTS_FAILED,
+  GET_EVENTS_SUCCESS,
+  SET_WORK_EXPERIENCE,
+} from '../actions.types';
 
 import { getEvents } from '../../../services/events';
 
+export const changeDPath = (value) => (dispatch) => {
+  console.log(value);
+  dispatch({
+    type: CHANGE_PAGE,
+    payload: value,
+  });
+};
 
-  export const changeDPath = (value)=> (dispatch) => {
-      console.log(value)
+export const events = (id) => async (dispatch) => {
+  try {
+    const res = await getEvents(id);
     dispatch({
-      type:CHANGE_PAGE,
-      payload:value
-    })
+      type: GET_EVENTS_SUCCESS,
+    });
 
-      
-}
-
-export  const events = (id) => async(dispatch) =>{
-    try{
-      const res = await getEvents(id);
-        dispatch({
-          type:GET_EVENTS_SUCCESS
-        })
-
-        return res;
-
-    }catch(err){
-      dispatch({
-        type: GET_EVENTS_FAILED
-      })
-    }
-}
+    return res;
+  } catch (err) {
+    dispatch({
+      type: GET_EVENTS_FAILED,
+    });
+  }
+};
 
 // const setAlert = ()=>{
 
 // }
 
-export const showDEventAction = ()=> (dispatch) => {
-
+export const showDEventAction = () => (dispatch) => {
   dispatch({
-    type:SHOW_EVENT,
-  })
-}
+    type: SHOW_EVENT,
+  });
+};
+
+export const setDWorkExperience = (values) => (dispatch) => {
+  dispatch({
+    type: SET_WORK_EXPERIENCE,
+    payload: values,
+  });
+};
