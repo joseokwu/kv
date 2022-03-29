1e7ea2f85c44e83dce6e56ef114d9b35f33e58b1
Jose <38558352+joseokwu@users.noreply.github.com>
2022-03-29 13:28:28 +0100

    work experience

--- src/Startupcomponents/workExperience/WorkExperience.js
+++ src/Startupcomponents/workExperience/WorkExperience.js
@@ -18,11 +18,17 @@ export const WorkExperience = ({
         <img src={appleSmall} alt='apple' />
       </div>
       <div className='info'>
-        <h5 className='title'>{title}</h5>
-        <h6 className='position'>{position}</h6>
-        <h6 className='country'>{country}</h6>
-        <h6 className='date'>{`${startDate} - ${endDate} 5 yrs`}</h6>
-        <p>{description}</p>
+        <h5 className='title'>Applane Insteen</h5>
+        <h6 className='position'>CEO Applane Insteen</h6>
+        <h6 className='country'>United States</h6>
+        <h6 className='date'>2015 - Present 5 yrs</h6>
+        <p>
+          My responsibility as the product lead is to ensure the success of our
+          product team. As a B2B product team, we help our clients realize their
+          product development goals. I work with the design and engineering
+          teams to craft and develop cutting edge software that meets market
+          standards.
+        </p>
       </div>
       <div className='buttons'>
         <img src={editIcon} alt='edit' /> <img src={deleteIcon} alt='delete' />
