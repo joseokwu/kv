80124da578f93cae8fe63a40b6c200832f81a37a
ozikelvin <isaackelvin378@gmail.com>
2022-04-10 20:15:06 +0100

    'qick-fix'

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -135,7 +135,7 @@ const routes = [
     path: '/startup/program',
     component: WithStartupDashboardLayout(StartupProgram),
     exact: true,
-    protected: false,
+    protected:true,
     type: 'startup',
   },
 
@@ -144,7 +144,7 @@ const routes = [
     path: '/startup/fundraising',
     component: WithStartupDashboardLayout(StartupFundingRaising),
     exact: true,
-    protected: false,
+    protected:true,
     type: 'startup',
   },
   {
@@ -152,7 +152,7 @@ const routes = [
     path: '/startup/boosterpartner',
     component: WithStartupDashboardLayout(StartupBoosterPartner),
     exact: true,
-    protected: false,
+    protected:true,
     type: 'startup',
   },
   {
@@ -160,7 +160,7 @@ const routes = [
     path: '/startup/e-academy',
     component: WithStartupDashboardLayout(StartupEAcademy),
     exact: true,
-    protected: false,
+    protected:true,
     type: 'startup',
   },
   {
@@ -168,7 +168,7 @@ const routes = [
     path: '/startup/todolist',
     component: WithStartupDashboardLayout(StartupTodoList),
     exact: true,
-    protected: false,
+    protected:true,
     type: 'startup',
   },
   {
@@ -176,7 +176,7 @@ const routes = [
     path: '/startup/networking',
     component: WithStartupDashboardLayout(StartupNetworking),
     exact: true,
-    protected: false,
+    protected:true,
     type: 'startup',
   },
   {
@@ -184,7 +184,7 @@ const routes = [
     path: '/startup/dealroom',
     component: WithStartupDashboardLayout(StartupDealRoom),
     exact: true,
-    protected: false,
+    protected:true,
     type: 'startup',
   },
   {
@@ -192,7 +192,7 @@ const routes = [
     path: '/startup/team/member',
     component: WithStartupRegisterLayout(StartupTeamMember),
     exact: true,
-    protected: false,
+    protected:true,
     type: 'startup',
   },
   {
@@ -200,7 +200,7 @@ const routes = [
     path: '/startup/events',
     component: WithStartupDashboardLayout(StartupEvents),
     exact: true,
-    protected: false,
+    protected:true,
     type: 'startup',
   },
   {
@@ -208,7 +208,7 @@ const routes = [
     path: '/startup/support',
     component: WithStartupDashboardLayout(StartupContactUs),
     exact: true,
-    protected: false,
+    protected:true,
     type: 'startup',
   },
   {
@@ -246,7 +246,7 @@ const routes = [
     path: '/investor/dashboard',
     component: WithMainInvestorLayout(InvestorDashboard),
     exact: true,
-    protected: false,
+    protected:false,
     type: 'investor',
   },
 
