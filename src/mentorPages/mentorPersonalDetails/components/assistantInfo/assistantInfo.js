0e0872d986cdb57d85375b773148e062aba2d6cd
nonicalx <nonicalx@gmail.com>
2022-04-21 13:24:53 +0100

    add handle submit & handle chnage

--- src/mentorPages/mentorPersonalDetails/components/assistantInfo/assistantInfo.js
+++ src/mentorPages/mentorPersonalDetails/components/assistantInfo/assistantInfo.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import toast from "react-hot-toast";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import {
@@ -15,7 +16,25 @@ import { useAuth } from "../../../../hooks";
 const AssistantInfo = () => {
   const { goBack, push } = useHistory();
 
-  const { stateAuth } = useAuth();
+  const [loading, setLoading] = useState(false);
+
+  const { stateAuth, updateMentorProfileState, updateMentorInfo } = useAuth();
+
+  const handleSubmit = async () => {
+    setLoading(true);
+
+    const uploaded = await updateMentorInfo();
+
+    if (uploaded) {
+      toast.success("Submitted Successfully");
+    } else {
+      toast.error("Something went wrong");
+    }
+    if (uploaded) {
+      push("/mentor/dashboard");
+    }
+    setLoading(false);
+  };
 
   const formik = useFormik({
     enableReinitialize: true,
@@ -49,10 +68,30 @@ const AssistantInfo = () => {
       assistantCity: Yup.string().required("This field is required"),
       assistantState: Yup.string().required("This field is required"),
     }),
+    onSubmit: (values) => handleSubmit(),
   });
 
+  const handleChange = (e, prefix = "") => {
+    const { name, value } = e.target;
+    if (prefix !== "") {
+      updateMentorProfileState("assistantInfo", {
+        [prefix]: {
+          ...stateAuth?.mentorData?.assistantInfo[prefix],
+          [name]: value,
+        },
+      });
+      formik.handleChange(e);
+      return;
+    }
+
+    updateMentorProfileState("assistantInfo", {
+      [name]: value,
+    });
+    formik.handleChange(e);
+  };
+
   return (
-    <div className="mentor_details_form_wrap">
+    <form className="mentor_details_form_wrap" onSubmit={formik.handleSubmit}>
       <h3>Assistant Info</h3>
       <p>This will help us personalise your preferences</p>
       <FormCard>
@@ -63,37 +102,73 @@ const AssistantInfo = () => {
         <div className="row">
           <section className="col-md-6 mb-4">
             <label>First Name*</label>
-            <TextField placeholder={"Micheal"} required={true} />
+            <TextField
+              placeholder={"Micheal"}
+              required={true}
+              name="assistantFirstname"
+              onChange={handleChange}
+              value={formik.values.assistantFirstname}
+            />
+            {formik.touched.assistantFirstname &&
+            formik.errors.assistantFirstname ? (
+              <label className="error">
+                {formik.errors.assistantFirstname}
+              </label>
+            ) : null}
           </section>
           <section className="col-md-6 mb-4">
             <label>Last Name*</label>
             <TextField
-              // label={'Last Name*'}
               placeholder={"Smith"}
               required={true}
+              name="assistantLastname"
+              onChange={handleChange}
+              value={formik.values.assistantLastname}
             />
+            {formik.touched.assistantLastname &&
+            formik.errors.assistantLastname ? (
+              <label className="error">{formik.errors.assistantLastname}</label>
+            ) : null}
           </section>
 
           <section className="col-md-4 mb-4">
             <label>Country</label>
             <TextField
-              // label={'Country'}
               placeholder={"Enter your country"}
+              name="assistantCountry"
+              onChange={handleChange}
+              value={formik.values.assistantCountry}
             />
+            {formik.touched.assistantCountry &&
+            formik.errors.assistantCountry ? (
+              <label className="error">{formik.errors.assistantCountry}</label>
+            ) : null}
           </section>
           <section className="col-md-4 mb-4">
             <label>State</label>
             <TextField
               // label={'State'}
               placeholder={"Enter your state"}
+              name="assistantState"
+              onChange={handleChange}
+              value={formik.values.assistantState}
             />
+            {formik.touched.assistantState && formik.errors.assistantState ? (
+              <label className="error">{formik.errors.assistantState}</label>
+            ) : null}
           </section>
           <section className="col-md-4 mb-4">
             <label>City</label>
             <TextField
               // label={'City'}
               placeholder={"Enter your city"}
+              name="assistantCity"
+              onChange={handleChange}
+              value={formik.values.assistantCity}
             />
+            {formik.touched.assistantCity && formik.errors.assistantCity ? (
+              <label className="error">{formik.errors.assistantCity}</label>
+            ) : null}
           </section>
 
           <section className="col-md-12 mb-4">
@@ -102,19 +177,44 @@ const AssistantInfo = () => {
               placeholder={"Enter your permanent address"}
               required={true}
               rows={"1"}
+              onChange={handleChange}
+              name="assistantAddress"
+              value={formik.values.assistantAddress}
             />
+            {formik.touched.assistantAddress &&
+            formik.errors.assistantAddress ? (
+              <label className="error">{formik.errors.assistantAddress}</label>
+            ) : null}
           </section>
 
           <section className="col-md-6 mb-4">
             <label className="mb-3">Email*</label>
             <TextField
-              // label={'Email*'}
               placeholder={"Michealsmith@gmail.com"}
+              type="email"
               required={true}
+              onChange={handleChange}
+              name="assistantEmail"
+              value={formik.values.assistantEmail}
             />
+            {formik.errors.assistantEmail ? (
+              <label className="error">{formik.errors.assistantEmail}</label>
+            ) : null}
           </section>
           <section className="col-md-6 mb-4">
-            <PhoneInput label="Mobile Number" />
+            <PhoneInput
+              label="Mobile Number"
+              name="assistantPhone"
+              value={formik.values.assistantPhone}
+              onChange={(e) =>
+                handleChange({
+                  target: { name: "assistantPhone", value: e.id },
+                })
+              }
+            />
+            {formik.errors.assistantPhone ? (
+              <label className="error">{formik.errors.assistantPhone}</label>
+            ) : null}
           </section>
         </div>
       </FormCard>
@@ -123,17 +223,17 @@ const AssistantInfo = () => {
         <button
           className="back-btn"
           onClick={() => {
-            goBack();
+            push("#consulting");
           }}
         >
           Go Back
         </button>
 
         <div className="d-flex align-items-center" style={{ columnGap: 9 }}>
-          <Button label="Submit" onClick={() => push("/mentor/dashboard")} />
+          <Button label="Submit" type="submit" />
         </div>
       </section>
-    </div>
+    </form>
   );
 };
 
