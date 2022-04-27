8b0fc040c692087344d90fc311710c5f7f93299a
ozikelvin <isaackelvin378@gmail.com>
2022-04-27 16:30:56 +0100

    'quick-fix'

--- src/Investorpages/profile/Profile.js
+++ src/Investorpages/profile/Profile.js
@@ -1,4 +1,4 @@
-import React , { useEffect , useState } from "react";
+import React  from "react";
 import ProfileDetails from "./components/profileDetials/ProfileDetails";
 import ProfileOfferings from "./components/profileOfferings/ProfileOfferings";
 import "./profile.css";
