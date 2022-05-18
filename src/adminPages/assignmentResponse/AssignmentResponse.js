b886c40aa5e7c14513be02a8322ee0d7549c3c49
nonicalx <nonicalx@gmail.com>
2022-05-18 13:48:40 +0100

    program completed

--- src/adminPages/assignmentResponse/AssignmentResponse.js
+++ src/adminPages/assignmentResponse/AssignmentResponse.js
@@ -5,8 +5,24 @@ import down from "../../assets/icons/downArrow.svg";
 import styles from "./response.module.css";
 import { AssignmentFile } from "../../adminComponents";
 import { AddNote } from "./components/AddNote";
+import { useHistory } from "react-router-dom";
 
 export const AssignmentResponse = () => {
+  const {
+    location: { hash },
+  } = useHistory();
+
+  console.log("hash", hash);
+
+  const startupResponses = [
+    { status: "pending", id: 123 },
+    { status: "completed", id: 345 },
+    { status: "pending", id: 567 },
+    { status: "completed", id: 756 },
+    { status: "pending", id: 980 },
+    { status: "completed", id: 890 },
+  ];
+
   return (
     <div className="p-5 bg-white" style={{ minHeight: "93vh" }}>
       <GoBack />
@@ -19,16 +35,23 @@ export const AssignmentResponse = () => {
       </section>
 
       <section className="pt-2">
-        {Array.from("two").map((t, i) => (
-          <StartupResponse key={`response-${i}`} />
-        ))}
+        {startupResponses?.length > 0 ? (
+          startupResponses
+            ?.filter((s) => `#${s.status}` === hash.toLowerCase())
+            ?.map((t, i) => (
+              <StartupResponse key={`response-${i}`} response={t} />
+            ))
+        ) : (
+          <h3 className="text-center mt-4">No Responses</h3>
+        )}
       </section>
     </div>
   );
 };
 
-const StartupResponse = () => {
+const StartupResponse = ({ response = {} }) => {
   const [open, setOpen] = useState(false);
+  const { push } = useHistory();
   return (
     <div style={{ marginBottom: "2rem" }}>
       <Modal title="Add a note" id="addNote">
@@ -62,12 +85,22 @@ const StartupResponse = () => {
                   consectetur adipiscing elit. Enim lectus morbi elementum
                   eu.Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
                 </p>
-                <Button
-                  label="Give Feedback"
-                  variant="secondary"
-                  data-target="#addNote"
-                  data-toggle="modal"
-                />
+                {response?.status === "pending" ? (
+                  <Button
+                    label="Give Feedback"
+                    variant="secondary"
+                    data-target="#addNote"
+                    data-toggle="modal"
+                  />
+                ) : (
+                  <Button
+                    label="View Feedback"
+                    variant="secondary"
+                    onClick={() =>
+                      push(`/admin/program/feedback/${response?.id}`)
+                    }
+                  />
+                )}
               </div>
             </section>
             <section className="col-lg-4">
