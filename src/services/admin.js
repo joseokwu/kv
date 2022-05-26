f3a2d952442456238d8f4de2d1504db6ec92c6fb
nonicalx <nonicalx@gmail.com>
2022-05-26 11:45:16 +0100

    add mentor integration

--- src/services/admin.js
+++ src/services/admin.js
@@ -1,22 +1,31 @@
 import { request } from "../utils/axios";
 
-export const getStakeHolders = async(values) =>{
-    try{    
-    const response = await request.post('allStakeHolders', values)
+export const getStakeHolders = async (values) => {
+  try {
+    const response = await request.post("allStakeHolders", values);
     return response.data;
-    }catch(err){
-        console.log(err?.response?.data?.message)
-        throw err;
-    }
-}
+  } catch (err) {
+    console.log(err?.response?.data?.message);
+    throw err;
+  }
+};
 
-
-export const applicationManagement = async(values) =>{
-    try{    
-    const response = await request.post('manageApplications', values)
+export const applicationManagement = async (values) => {
+  try {
+    const response = await request.post("manageApplications", values);
     return response.data;
-    }catch(err){
-        console.log(err?.response?.data?.message)
-        throw err;
-    }
-}
+  } catch (err) {
+    console.log(err?.response?.data?.message);
+    throw err;
+  }
+};
+
+export const addMentor = async (values) => {
+  try {
+    const response = await request.post("addMentors", values);
+    return response.data;
+  } catch (err) {
+    console.log("serdsd", err?.response?.data?.message);
+    throw err;
+  }
+};
