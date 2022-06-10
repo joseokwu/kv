3ef681a11d7722e31313b1ece57952a9062e5c0e
Jose <38558352+joseokwu@users.noreply.github.com>
2022-06-10 09:18:50 +0100

    updates

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,5 @@
- export const BASE_URL = 'http://localhost:8085/';
+// export const BASE_URL = 'http://localhost:8085/';
+export const BASE_URL = 'https://fca9-102-89-42-194.ngrok.io';
 
 //http://a86f4fc2bf8b347d191ece7f35409017-724927569.eu-central-1.elb.amazonaws.com/
 
@@ -7,4 +8,4 @@
 //https://d20kwg47th11r1.cloudfront.net
 
 //  export const BASE_URL = "http://bd3c-129-56-33-20.ngrok.io";
- // export const BASE_URL = "https://d20kwg47th11r1.cloudfront.net/";
+// export const BASE_URL = "https://d20kwg47th11r1.cloudfront.net/";
