f45fe286792f85ce5068044deccdcc0dc613318e
Jose <38558352+joseokwu@users.noreply.github.com>
2022-06-13 17:12:56 +0100

    updates

--- src/config/env.js
+++ src/config/env.js
@@ -8,5 +8,5 @@
 
 //https://d20kwg47th11r1.cloudfront.net
 
-export const BASE_URL = 'http://fb4d-129-56-64-241.ngrok.io';
+export const BASE_URL = 'https://4f18-102-89-41-234.ngrok.io/';
 // export const BASE_URL = "https://d20kwg47th11r1.cloudfront.net/";
