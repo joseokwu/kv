2d7584729f3526be16df74e3c4bd83182d9e1f94
elijahthis <emperorelijah007@gmail.com>
2022-07-01 11:09:56 +0100

    pagination

--- src/adminPages/applicationMgt/components/PendingTable.js
+++ src/adminPages/applicationMgt/components/PendingTable.js
@@ -1,77 +1,101 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { Table } from "../../../adminComponents";
 import { Tag } from "../../../components";
 import { formatDate } from "../../../utils/helpers";
 import left from "../../../assets/icons/chervonLeft.svg";
 import styles from "../applicationMgt.module.css";
-import { EmptyState } from './../../../mentorComponents/emptyState/EmptyState';
+import { EmptyState } from "./../../../mentorComponents/emptyState/EmptyState";
+import { PaginationData } from "../../../components";
 
+export const PendingTable = ({
+    applications,
+    currentPending,
+    setCurrentPending,
+}) => {
+    const [currentPage, setCurrentPage] = useState(1);
 
+    let limit = 5;
+    let visible = [];
 
-export const PendingTable = ({applications, currentPending , setCurrentPending}) => {
+    if (applications?.startups) {
+        if (applications?.startups.length < limit)
+            visible = applications?.startups;
+        else
+            visible = applications?.startups.slice(
+                limit * (currentPage - 1),
+                limit * (currentPage - 1) + limit
+            );
+    }
 
+    const header = useMemo(
+        () => [
+            {
+                title: "Startup",
+                accessor: "startup",
+            },
+            {
+                title: "Application Date",
+                accessor: "date",
+            },
+            {
+                title: "Status",
+                accessor: "status",
+            },
+            {
+                title: "Action",
+                accessor: "action",
+            },
+        ],
+        []
+    );
 
+    const applicationData = useMemo(() =>
+        visible?.map((item, i) => {
+            return {
+                startup: (
+                    <div className="d-flex align-items-center space-out">
+                        <img
+                            src={item?.startUpProfile?.logo}
+                            alt="user"
+                            className={styles.userPic}
+                        />
+                        <p className="mb-0">
+                            {item?.startUpProfile?.acceleratorName}
+                        </p>
+                    </div>
+                ),
 
-  const header = useMemo(
-    () => [
-      {
-        title: "Startup",
-        accessor: "startup",
-      },
-      {
-        title: "Application Date",
-        accessor: "date",
-      },
-      {
-        title: "Status",
-        accessor: "status",
-      },
-      {
-        title: "Action",
-        accessor: "action",
-      },
-    ],
-    []
-  );
+                date: formatDate(new Date(item?.startUpProfile?.yearFounded)),
 
+                status: <Tag name="Pending" color="#2E3192" />,
 
+                action: (
+                    <Link
+                        to={`/admin/application_mgt/pending/${item?.userId}`}
+                        className="view-link"
+                    >
+                        View
+                    </Link>
+                ),
+            };
+        }, [])
+    );
 
-  const applicationData = useMemo(
-    () => applications?.startups?.map((item , i) =>{
-      return  {
-        startup: (
-          <div className="d-flex align-items-center space-out">
-            <img src={item?.startUpProfile?.logo} alt="user" className={styles.userPic} />
-            <p className="mb-0">{ item?.startUpProfile?.acceleratorName }</p>
-          </div>
-        ),
+    if (applications?.startups?.length === 0) {
+        return <EmptyState message="No Application yet." />;
+    }
 
-        date: formatDate(new Date(item?.startUpProfile?.yearFounded)),
-
-        status: <Tag name="Pending" color="#2E3192" />,
-
-        action: (
-          <Link to={`/admin/application_mgt/pending/${item?.userId}`} className="view-link">
-            View
-          </Link>
-        ),
-      }
-    }, [])
-
-    
-  )
-
-
-
-  if(applications?.startups?.length === 0){
-    return <EmptyState message="No Application yet." />
-  }
-
-  return (
-    <div>
-      <Table headers={header} data={applicationData} />
-       {/* Pagination goes here */}
-    </div>
-  );
+    return (
+        <div>
+            <Table headers={header} data={applicationData} />
+            <PaginationData
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                data={applications?.startups || []}
+                limit={limit}
+            />
+            {/* Pagination goes here */}
+        </div>
+    );
 };
--- src/config/env.js
+++ src/config/env.js
@@ -5,4 +5,4 @@
 //export const BASE_URL = "http://fb4d-129-56-64-241.ngrok.io";
 
 export const BASE_URL = "https://d20kwg47th11r1.cloudfront.net/";
-
+// export const BASE_URL = "https://prod.knight.ventures/";
