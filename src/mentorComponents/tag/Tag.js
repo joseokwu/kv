35fae4e2a07f5b433ddb4857e05e4f74964b7217
elijahthis <emperorelijah007@gmail.com>
2022-08-16 10:45:03 +0100

    more little changes

--- src/mentorComponents/tag/Tag.js
+++ src/mentorComponents/tag/Tag.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./tag.css";
 
-export const Tag = ({ name = "tag", color = "#058DC1"}) => {
-  return (
-    <span
-      className="tag-main"
-      style={{ backgroundColor: `${color}20`, color: color }}
-    >
-      {name}
-    </span>
-  );
+export const Tag = ({ name = "tag", color = "#058DC1" }) => {
+    return (
+        <span
+            className="tag-main"
+            style={{ backgroundColor: `${color}20`, color: color }}
+        >
+            {name}
+        </span>
+    );
+};
+
+export const Tag2 = ({ name = "tag", color = "#058DC1" }) => {
+    return (
+        <span
+            className="tag-sec"
+            style={{ backgroundColor: `${color}20`, color: color }}
+        >
+            {name}
+        </span>
+    );
 };
